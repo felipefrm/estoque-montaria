68496b3c891612d28cadb62e34be53c4ce184e07
ThalesMrad <39007379+ThalesMrad@users.noreply.github.com>
2019-10-17 23:18:28 -0300

    Add files via upload

--- Estoque.java
+++ Estoque.java
@@ -1,14 +1,16 @@
 package tp1;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class Estoque implements EstoqueInterface{
+public class Estoque extends EstoqueInterface{
 	private int quantidade;
 	private float preco;
 	private Montaria montaria;
-	Scanner sc = new Scanner(System.in);
 	
+	public Montaria getMontaria() {
+		return montaria;
+	}
+	public void setMontaria(Montaria montaria) {
+		this.montaria = montaria;
+	}
 	public int getQuantidade() {
 		return quantidade;
 	}
@@ -21,75 +23,5 @@ public class Estoque implements EstoqueInterface{
 	public void setPreco(float preco) {
 		this.preco = preco;
 	}
-	public void editaEstoque(ArrayList<Estoque> estoque) {
-		int opcao = 0;
-		String racaEscolha ="";
-		
-		while(true) {
-			System.out.printf("Edita Estoque");
-			System.out.printf("Deseja: \n 1-Editar um existente \n 2-Adicionar um novo? \n 0-Sair");
-			sc.nextInt(opcao);
-			
-			if(opcao == 0)
-				break;
-			
-			if(opcao == 1) {
-				int j = 0,i = 0;
-				System.out.printf("Estoque de qual raça deseja editar?");
-				sc.next(racaEscolha);
-				
-				while(estoque.get(i).montaria.getRaca() != racaEscolha) {
-					i++;
-				}
-				System.out.println("O que deseja fazer com esse estoque?");
-				System.out.println("1:Remover estoque");
-				System.out.println("2:Editar quantidade");
-				System.out.println("0:Voltar");
-				sc.nextInt(j);
-				
-				switch(j) {
-				case 1:
-					estoque.remove(i);
-					break;
-				case 2:
-					int qtd = 0;
-					System.out.printf("Atual quantidade: "+estoque.get(i).getQuantidade());
-					System.out.printf("Quer alterar para quanto?");
-					sc.nextInt(qtd);
-					if(qtd == 0)
-						System.out.println("Quantidade inválida, tente remover o estoque.");
-					else {
-						estoque.get(i).setQuantidade(qtd);
-						System.out.println("Quantidade alterada com sucesso!");
-					}
-					break;
-				case 0:
-					break;
-					
-				}
-				
-			}
-			
-		}
-	}
-	public void visualizaEstoque(ArrayList<Estoque> estoque) {
-		int opcao = 0,i = 0;
-		String racaEscolha = "";
-		while(true) {
-			System.out.printf("O que deseja?\n 0 - Sair \n 1 - Vizualizar um estoque\n");
-			sc.nextInt(opcao);
-			if(opcao == 0)
-				break;
-			else
-				
-				System.out.printf("Estoque de qual raça deseja visualizar?");
-				sc.next(racaEscolha);
-				while(estoque.get(i).montaria.getRaca() != racaEscolha) {
-					i++;
-				}
-				System.out.println("Estoque da raça: "+estoque.get(i).montaria.getRaca());
-				System.out.println("Quantidade disponivel: "+estoque.get(i).getQuantidade());
-				System.out.println("Preço individual da montaria: "+estoque.get(i).getPreco());
-		}
-	}
+	
 }
--- EstoqueInterface.java
+++ EstoqueInterface.java
@@ -1,9 +1,79 @@
 package tp1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public interface EstoqueInterface {
-	public void editaEstoque(ArrayList<Estoque> estoque);
-	public void visualizaEstoque(ArrayList<Estoque> estoque);
+public abstract class EstoqueInterface {
+	Scanner sc = new Scanner(System.in);
+	public void editaEstoque(ArrayList<Estoque> estoque) {
+		int opcao = 0;
+		String racaEscolha ="";	
+		while(true) {
+			System.out.printf("Edita Estoque");
+			System.out.printf("Deseja: \n 1-Editar um existente \n 2-Adicionar um novo? \n 0-Sair");
+			sc.nextInt(opcao);
+			
+			if(opcao == 0)
+				break;
+			
+			if(opcao == 1) {
+				int j = 0,i = 0;
+				System.out.printf("Estoque de qual raça deseja editar?");
+				sc.next(racaEscolha);
+				
+				while(estoque.get(i).getMontaria().getRaca() != racaEscolha) {
+					i++;
+				}
+				System.out.println("O que deseja fazer com esse estoque?");
+				System.out.println("1:Remover estoque");
+				System.out.println("2:Editar quantidade");
+				System.out.println("0:Voltar");
+				sc.nextInt(j);
+				
+				switch(j) {
+				case 1:
+					estoque.remove(i);
+					break;
+				case 2:
+					int qtd = 0;
+					System.out.printf("Atual quantidade: "+estoque.get(i).getQuantidade());
+					System.out.printf("Quer alterar para quanto?");
+					sc.nextInt(qtd);
+					if(qtd == 0)
+						System.out.println("Quantidade inválida, tente remover o estoque.");
+					else {
+						estoque.get(i).setQuantidade(qtd);
+						System.out.println("Quantidade alterada com sucesso!");
+					}
+					break;
+				case 0:
+					break;
+					
+				}
+				
+			}
+			
+		}
+	}
+	public void visualizaEstoque(ArrayList<Estoque> estoque) {
+		int opcao = 0,i = 0;
+		String racaEscolha = "";
+		while(true) {
+			System.out.printf("O que deseja?\n 0 - Sair \n 1 - Vizualizar um estoque\n");
+			sc.nextInt(opcao);
+			if(opcao == 0)
+				break;
+			else
+				
+				System.out.printf("Estoque de qual raça deseja visualizar?");
+				sc.next(racaEscolha);
+				while(estoque.get(i).getMontaria().getRaca() != racaEscolha) {
+					i++;
+				}
+				System.out.println("Estoque da raça: "+estoque.get(i).getMontaria().getRaca());
+				System.out.println("Quantidade disponivel: "+estoque.get(i).getQuantidade());
+				System.out.println("Preço individual da montaria: "+estoque.get(i).getPreco());
+		}
+	}
 	
 }
