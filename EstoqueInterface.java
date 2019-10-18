873f4bd57e92fc143c9d3ee99ab46be1b04db719
ThalesMrad <39007379+ThalesMrad@users.noreply.github.com>
2019-10-17 22:34:09 -0300

    Add files via upload

---
+++ EstoqueInterface.java
@@ -0,0 +1,9 @@
+package tp1;
+
+import java.util.ArrayList;
+
+public interface EstoqueInterface {
+	public void editaEstoque(ArrayList<Estoque> estoque);
+	public void visualizaEstoque(ArrayList<Estoque> estoque);
+	
+}
