a3c011174ada21281d853d129224b1dc3a271512
heitor57 <heitorwerneck@hotmail.com>
2019-10-29 13:03:38 -0300

    __main__, Utils

--- python/Main.py
+++ python/Main.py
@@ -2,88 +2,86 @@ from MontariaInterface import MontariaInterface
 from VendedorInterface import VendedorInterface
 from Montaria import Montaria
 from Vendedor import Vendedor
-import re
+from Utils import Utils
+
 import json
 
-vendedores = []
-montarias = []
-def getObjDict(obj):
-    obj_dict=obj.__dict__
-    for i in obj_dict.copy():
-        name=re.split("__", i)[-1]
-        obj_dict[name]=obj_dict.pop(i)
-    return obj_dict
-print("╔════════ Bases de dados ════════╗")
-fname_vend=input("Nome do arquivo de vendedores ⟶ ")
-max_id=1
-list_dict=list()
-try:
-    with open(fname_vend, 'r') as fvend:
-        list_dict=json.load(fvend)
-        for obj in list_dict:
-            max_id=max(max_id,obj['id']+1)
-            vendedores.append(Vendedor(obj['nome'], obj['raca'], obj['idade'], obj['descricao']))
-            vendedores[-1].setId(obj['id'])
-            estoque=list()
-            for i in obj['estoque']:
-                estoque.append(Estoque(i['quantidade'],i['preco'],i['montaria']))
-            vendedores[-1].setEstoque(estoque)
-    Vendedor.setCont(max_id)
-except Exception as e:
-    print(e)
-    print("Arquivo não encontrado. Usando base vazia.")
-print(str(len(vendedores))+" vendedores carregados da base")
-
-fname_mont=input("Nome do arquivo de montarias ⟶ ")
-max_id=1
-list_dict=list()
-try:
-    with open(fname_mont, 'r') as fvend:
-        list_dict=json.load(fvend)
-        for obj in list_dict:
-            max_id=max(max_id,obj['id']+1)
-            montarias.append(Montaria(obj['nome'], obj['raca'], obj['descricao'], obj['combustivel'], 
-            obj['raridade'], obj['capacidade'], obj['velocidade']))
-            montarias[-1].setId(obj['id'])
-    Montaria.setCont(max_id)
-except Exception as e:
-    print(e)
-    print("Arquivo não encontrado. Usando base vazia.")
-print(str(len(montarias))+" montarias carregadas da base")
 
 
-while True:
-    print("\n╔════════ MENU ════════╗")
+if __name__ == '__main__':
+    vendedores = []
+    montarias = []
+    print("╔════════ Bases de dados ════════╗")
+    fname_vend=input("Nome do arquivo de vendedores ⟶ ")
+    max_id=1
+    list_dict=list()
     try:
-        op = int(input("╠0 Fechar programa\n╠1 Menu Vendedor\n╚2 Menu Montaria\n⟶ "))
-    except ValueError:
-        continue
-    if op == 0:
-        list_dict=list()
-        if vendedores:
-            for v in vendedores:
-                obj_dict=getObjDict(v)
-                ests=list()
-                for i in obj_dict['estoque']:
-                    ests.append(getObjDict(i))
-                    ests[-1]['montaria']=getObjDict(ests[-1]['montaria'])
-                obj_dict['estoque']=ests
-                list_dict.append(obj_dict)
-            with open(fname_vend, 'w') as outfile:
-                json.dump(list_dict, outfile)
-        list_dict=list()
-        
-        if montarias:
-            for m in montarias:
-                list_dict.append(getObjDict(m))
-            with open(fname_mont, 'w') as outfile:
-                json.dump(list_dict, outfile)
-        break
-    elif op == 1:
-        VendedorInterface.menu(vendedores, montarias)
+        with open(fname_vend, 'r') as fvend:
+            list_dict=json.load(fvend)
+            for obj in list_dict:
+                max_id=max(max_id,obj['id']+1)
+                vendedores.append(Vendedor(obj['nome'], obj['raca'], obj['idade'], obj['descricao']))
+                vendedores[-1].setId(obj['id'])
+                estoque=list()
+                for i in obj['estoque']:
+                    estoque.append(Estoque(i['quantidade'],i['preco'],i['montaria']))
+                vendedores[-1].setEstoque(estoque)
+        Vendedor.setCont(max_id)
+    except Exception as e:
+        print(e)
+        print("Arquivo não encontrado. Usando base vazia.")
+    print(str(len(vendedores))+" vendedores carregados da base")
 
-    elif op == 2:
-        MontariaInterface.menu(montarias)
+    fname_mont=input("Nome do arquivo de montarias ⟶ ")
+    max_id=1
+    list_dict=list()
+    try:
+        with open(fname_mont, 'r') as fvend:
+            list_dict=json.load(fvend)
+            for obj in list_dict:
+                max_id=max(max_id,obj['id']+1)
+                montarias.append(Montaria(obj['nome'], obj['raca'], obj['descricao'], obj['combustivel'], 
+                obj['raridade'], obj['capacidade'], obj['velocidade']))
+                montarias[-1].setId(obj['id'])
+        Montaria.setCont(max_id)
+    except Exception as e:
+        print(e)
+        print("Arquivo não encontrado. Usando base vazia.")
+    print(str(len(montarias))+" montarias carregadas da base")
 
-    else:
-        print("\nNão há esta opção, por favor digite novamente.")
+
+    while True:
+        print("\n╔════════ MENU ════════╗")
+        try:
+            op = int(input("╠0 Fechar programa\n╠1 Menu Vendedor\n╚2 Menu Montaria\n⟶ "))
+        except ValueError:
+            continue
+        if op == 0:
+            list_dict=list()
+            if vendedores:
+                for v in vendedores:
+                    obj_dict=Utils.getObjDict(v)
+                    ests=list()
+                    for i in obj_dict['estoque']:
+                        ests.append(Utils.getObjDict(i))
+                        ests[-1]['montaria']=Utils.getObjDict(ests[-1]['montaria'])
+                    obj_dict['estoque']=ests
+                    list_dict.append(obj_dict)
+                with open(fname_vend, 'w') as outfile:
+                    json.dump(list_dict, outfile)
+            list_dict=list()
+            
+            if montarias:
+                for m in montarias:
+                    list_dict.append(Utils.getObjDict(m))
+                with open(fname_mont, 'w') as outfile:
+                    json.dump(list_dict, outfile)
+            break
+        elif op == 1:
+            VendedorInterface.menu(vendedores, montarias)
+
+        elif op == 2:
+            MontariaInterface.menu(montarias)
+
+        else:
+            print("\nNão há esta opção, por favor digite novamente.")
---
+++ python/Utils.py
@@ -0,0 +1,19 @@
+from abc import ABC,abstractmethod
+
+import re
+class Utils(ABC):
+    @staticmethod
+    def inputER(arg='',typeC=str):
+        while True:
+            try:
+                r=typeC(input(arg))
+            except ValueError:
+                continue
+        return r
+    @staticmethod
+    def getObjDict(obj):
+        obj_dict=obj.__dict__
+        for i in obj_dict.copy():
+            name=re.split("__", i)[-1]
+            obj_dict[name]=obj_dict.pop(i)
+        return obj_dict
