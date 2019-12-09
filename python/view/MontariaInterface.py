680df777c934cd6092f35ce6162f0e6c72dc4a5f
ThalesMrad <39007379+ThalesMrad@users.noreply.github.com>
2019-12-08 21:08:55 -0300

    clear

--- python/view/MontariaInterface.py
+++ python/view/MontariaInterface.py
@@ -1,6 +1,9 @@
 from tkinter import*
 from tkinter import ttk
 import tkinter as tk
+from Montaria import Montaria
+from Utils import Utils
+
 class MontariaInterface:
     def __init__(self, instancia_Tk):
 
@@ -8,9 +11,11 @@ class MontariaInterface:
         instancia_Tk.title("Montaria")
         instancia_Tk.minsize(440,400)
 
-        self.dataCols = ('ID', 'Raça','currency')
+        self.dataCols = ('ID', 'Raça','Combustivel','Raridade','Capacidade','Velocidade')
         self.tree = ttk.Treeview(columns=self.dataCols, show='headings')
         self.tree.grid(row=7, column=3, sticky= tk.N + tk.S + tk.W + tk.E)
+        for col in self.dataCols:
+            self.tree.heading(col,text=col)
 
 
         fonte1 = ('Verdana', '10', 'bold')
@@ -49,7 +54,7 @@ class MontariaInterface:
         self.velocidade.grid(row=5, column=1)
 
 
-        self.botaoAdd = Button(instancia_Tk, text ='Adicionar')
+        self.botaoAdd = Button(instancia_Tk, text ='Adicionar',command = self.add)
         self.botaoAdd['height'] = 2
         self.botaoAdd.grid(row=6, column=0)
 
@@ -57,9 +62,30 @@ class MontariaInterface:
         self.botaoRem['height'] = 2
         self.botaoRem.grid(row=6, column=1)
 
-        self.botaoLim = Button(instancia_Tk, text ='Limpar')
+        self.botaoLim = Button(instancia_Tk, text ='Limpar',command = self.clear)
         self.botaoLim['height'] = 2
         self.botaoLim.grid(row=6, column=2)
+
+    def criaMontaria(self):
+        raca = self.raca.get()
+        combustivel = self.combustivel.get()
+        raridade = self.raridade.get()
+        capacidade = self.capacidade.get()
+        velocidade = self.velocidade.get()
+        return Montaria(raca, combustivel, raridade, capacidade, velocidade)
+
+    def add(self,montarias):
+        nova_montaria = criaMontaria()
+        montarias.append(nova_montaria)
+
+
+    def clear(self):
+        self.id.delete(0,'end')
+        self.raca.delete(0,'end')
+        self.combustivel.delete(0,'end')
+        self.raridade.delete(0,'end')
+        self.capacidade.delete(0,'end')
+        self.velocidade.delete(0,'end')
 
 raiz=Tk()
 MontariaInterface(raiz)
